7388dc08414d35a8fe69d12124ba9d9ea05486ee
dimovich85 <dimovich85@gmail.com>
2020-08-25 17:01:47 +0300

    Сделал баннер - слайдер

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+import $ from 'jquery';
+import 'slick-carousel';
+
+$(function(){
+    $('.banner-slider__container').slick({
+        dots: true,
+        arrows: false
+    });
+});
